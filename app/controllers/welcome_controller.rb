33809c763bf65a34d922fb394a282358a4975b6b
Rodrigo Andrade <rodrigoandradebcc@gmail.com>
2014-05-30 21:26:12 -0300

    Primeira Versão

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,6 @@
+class WelcomeController < ApplicationController
+  def index
+    @articles = Article.order(created_at: :desc).paginate(page: params[:page], per_page: 1)
+    @article_by_month = Article.all.group_by{|post| post.created_at.strftime("%B")}
+  end
+end
